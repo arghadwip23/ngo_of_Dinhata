a163267e3850a6ad0f4787b3af1f541996008fa7
arghadwip23 <arghadwip23@gmail.com>
2024-09-14 16:09:33 +0530

    fixed the overlaping issue

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Counter() {
   return (
-    <section className="px-1 py-16 mt-3 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 ">
+    <section className="  px-1 py-16 mt-3 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 ">
     <div className="grid grid-cols-2 gap-y-5 md:grid-cols-4 ">
       <div className="text-center flex flex-col ">
         <h6 className="text-3xl font-bold text-blue-500">
--- app/components/Hero.js
+++ app/components/Hero.js
@@ -9,7 +9,7 @@ export default function Hero() {
  const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 
   return (
-    <section className="flex  md:px-20 md:pt-0  md:h-2/3 bg-zinc-50 flex-col items-center  border-red-200 w-screen space-y-0 h-screen p-0 md:flex-row items-center p-0 justify-between bg-white">
+    <section className="flex flex-wrap md:flex-nowrap h-full   md:px-20 md:pt-0  md:h-2/3 bg-zinc-50 flex-col items-center  border-red-200 w-screen space-y-0 h-screen p-0 md:flex-row items-center p-0 justify-between bg-white">
       {/* Left Side: Text */}
       <div className="  p-10 m-0 md:w-1/2 p-0  w-full text-center md:text-left md:p-0 lg:p-4">
         <h1 className="text-5xl pt-5 md:text-6xl font-bold text-gray-900 mb-4">
@@ -24,8 +24,8 @@ export default function Hero() {
       </div>
 
       {/* Right Side: Image (Hidden on small screens) */}
-      <div className="flex justify-middle items-center ">
-      <IKImage urlEndpoint={urlEndpoint} path="hero.png" width={500} height={500} alt="Alt text" className='' />
+      <div className="flex justify-middle items-center  ">
+      <IKImage urlEndpoint={urlEndpoint} path="hero.png"  alt="Alt text" height={500} width={500} className='w-full h-auto' />
       </div>
     </section>
   )
