bc440545f4ea5495659a9e55ad2ae949fe37d405
arghadwip23 <arghadwip23@gmail.com>
2024-09-14 14:43:11 +0530

    just for checking

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -8,25 +8,25 @@ export default function Counter() {
         <h6 className="text-3xl font-bold text-blue-500">
           144K
         </h6>
-        <p className="font-bold">Downloads</p>
+        <p className="font-bold text-gray-600">Downloads</p>
       </div>
       <div className="text-center">
         <h6 className="text-3xl font-bold text-blue-500">
           32.1K
         </h6>
-        <p className="font-bold">Users</p>
+        <p className="font-bold text-gray-600">Users</p>
       </div>
       <div className="text-center">
         <h6 className="text-3xl font-bold text-blue-500">
           12.9K
         </h6>
-        <p className="font-bold">Subscribers</p>
+        <p className="font-bold text-gray-600">Subscribers</p>
       </div>
       <div className="text-center">
         <h6 className="text-3xl font-bold text-blue-500">
           24.5K
         </h6>
-        <p className="font-bold">Cookies</p>
+        <p className="font-bold text-gray-600">Cookies</p>
       </div>
     </div>
   </section>
