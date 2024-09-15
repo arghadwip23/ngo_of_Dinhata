9b296609fd9739aabed07960beb2966b8ec34b46
arghadwip23 <arghadwip23@gmail.com>
2024-09-15 15:52:18 +0530

    hi

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 //import Hero from "./components/Hero";
 import Counter from "./components/Counter";
-import Team from "./components/Team";
+
 import HeroS from "./components/HeroS";
 import Slider from "./components/Slider";
 import Teama from "./components/teama";
