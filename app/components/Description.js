3600311cf0c66fe9c2068f566ff5cd6d7bad1ec9
arghadwip23 <arghadwip23@gmail.com>
2024-09-23 18:16:15 +0530

    adding

--- app/components/Description.js
+++ app/components/Description.js
@@ -5,10 +5,10 @@ export default function Description() {
     <section className="p-4 mt-20 lg:p-8 bg-gray-100 text-gray-800">
 	<div className="container mx-auto space-y-12">
 		<div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-			<img src="https://picsum.photos/200" alt="" className="h-80 bg-gray-500 " />
+			<img src="https://picsum.photos/200" alt="" className="h-80 bg-gray-500 aspect-video" />
 			<div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-50">
-				<span className="text-xs uppercase dark:text-gray-600">Join, it's free</span>
-				<h3 className="text-3xl font-bold">We're not reinventing the wheel</h3>
+				<span className="text-xs uppercase dark:text-gray-600">Join, it&apos;s free</span>
+				<h3 className="text-3xl font-bold">We&apos;re not reinventing the wheel</h3>
 				<p className="my-6 dark:text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
 				<button type="button" className="self-start">Action</button>
 			</div>
@@ -16,7 +16,7 @@ export default function Description() {
 		<div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row-reverse">
 			<img src="https://picsum.photos/200" alt="" className="h-80 dark:bg-gray-500 aspect-video" />
 			<div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-50">
-				<span className="text-xs uppercase dark:text-gray-600">Join, it's free</span>
+				<span className="text-xs uppercase dark:text-gray-600">Join, it&apos;s free</span>
 				<h3 className="text-3xl font-bold">We're not reinventing the wheel</h3>
 				<p className="my-6 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
 				<button type="button" className="self-start">Action</button>
@@ -25,8 +25,8 @@ export default function Description() {
 		<div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
 			<img src="https://picsum.photos/200" alt="" className="h-80 dark:bg-gray-500 aspect-video" />
 			<div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-50">
-				<span className="text-xs uppercase dark:text-gray-600">Join, it's free</span>
-				<h3 className="text-3xl font-bold">We're not reinventing the wheel</h3>
+				<span className="text-xs uppercase dark:text-gray-600">Join, it&apos;s free</span>
+				<h3 className="text-3xl font-bold">We&apos;re not reinventing the wheel</h3>
 				<p className="my-6 dark:text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
 				<button type="button" className="self-start">Action</button>
 			</div>
