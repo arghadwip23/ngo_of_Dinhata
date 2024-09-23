a28cacbf57f4bc6712201589aef5756c14cd5992
arghadwip23 <arghadwip23@gmail.com>
2024-09-23 18:23:38 +0530

    adding 3

--- app/components/Description.js
+++ app/components/Description.js
@@ -17,7 +17,7 @@ export default function Description() {
 			<img src="https://picsum.photos/200" alt="" className="h-80 dark:bg-gray-500 aspect-video" />
 			<div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-50">
 				<span className="text-xs uppercase dark:text-gray-600">Join, it&apos;s free</span>
-				<h3 className="text-3xl font-bold">We're not reinventing the wheel</h3>
+				<h3 className="text-3xl font-bold">We&apos;re not reinventing the wheel</h3>
 				<p className="my-6 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
 				<button type="button" className="self-start">Action</button>
 			</div>
