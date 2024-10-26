bb1c8b9e3a2d910e920696be21ef6d683112c85f
arghadwip23 <arghadwip23@gmail.com>
2024-10-26 21:43:24 +0530

    description part added

--- app/components/Description.js
+++ app/components/Description.js
@@ -4,12 +4,12 @@ export default function Description() {
   return (
     <section className="p-4 mt-20 lg:p-8  text-gray-800">
 	<div className="container mx-auto space-y-12">
-		<div className="flex flex-col overflow-hidden rounded-md shadow-md lg:flex-row">
+		<div className="flex flex-col overflow-hidden rounded-md shadow-sm transition-all duration-300 hover:scale-110 hover:shadow-xl lg:flex-row">
 			<img src="https://arghadwip23.github.io/image/Group6.png" alt="" className=" bg-gray-500  md:h-80 aspect-auto " />
 			<div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-50">
-				<span className="text-xs uppercase dark:text-gray-600">Join, it&apos;s free</span>
-				<h3 className="text-3xl font-bold">We&apos;re not reinventing the wheel</h3>
-				<p className="my-6 dark:text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
+				
+				<h3 className="text-4xl counterTitle">আমরা কারা?</h3>
+				<p className="my-6 dark:text-gray-600 text-lg">আমরা দিনহাটার কিছু স্বার্থহীন সমাজসেবক , মূলত পেশাগত ভাবে বেশির ভাগই শিক্ষক। নিজেদের কর্মসূত্রে আমরা এখানকার মানুষের আবশ্যকতা , দুর্বলতা এবং কিছু দরিদ্র গোষ্ঠীর অসহায়তার সাথে অবগত হওয়ায় সমাজে বদল আনার জন্য নিজেদের যোগদান দেওয়ার লক্ষ্যে আরো দৃঢ় হয় যায় । যার ফলস্বরূপ ২০২০ সালে দায়বদ্ধ ওয়েলফেয়ার সোসাইটির প্রতিস্থাপন করা হয়.</p>
 				<button type="button" className="self-start">Action</button>
 			</div>
 		</div>
