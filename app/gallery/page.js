b3e429eb4a228e7388f7e104c94791b4c723ef63
arghadwip23 <arghadwip23@gmail.com>
2024-10-29 20:03:34 +0530

    fixing bug

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -53,7 +53,7 @@ export default function Gallery() {
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
           {imageUrls.map((image, index) => (
-            <div>
+            <div key={index}>
               <img
                 src={image.link}
                 alt={`Gallery Image ${index + 1}`}
