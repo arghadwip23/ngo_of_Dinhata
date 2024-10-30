72262c407edd4d2dc10fea512235c16901c47ecc
arghadwip23 <arghadwip23@gmail.com>
2024-10-30 22:19:39 +0530

    hig

--- app/team/page.js
+++ app/team/page.js
@@ -1,7 +1,7 @@
 'use client'
 import {React, useEffect, useState} from 'react';
-import { getFirestore,getDoc,collection } from "firebase/firestore";
-import app from '@/util/arghafire';
+// import { getFirestore,getDoc,collection } from "firebase/firestore";
+// import app from '@/util/arghafire';
 
 
 
