11070c72c4ba5cf898a891168a8e8c3594076444
arghadwip23 <arghadwip23@gmail.com>
2024-10-30 21:10:44 +0530

    ju

--- app/team/page.js
+++ app/team/page.js
@@ -7,17 +7,17 @@ import { getDocs, collection } from "firebase/firestore";
 export default function Team() {
    
 
-    const [teamMembers,setTeam] = useState([]);
-    useEffect(()=>{
-        const getTeams = async ()=>{
-            const snap = await getDocs(collection(teamdata,"team"));
-            await setTeam(snap.docs);
-            console.log(snap.docs[0].data());
+    // const [teamMembers,setTeam] = useState([]);
+    // useEffect(()=>{
+    //     const getTeams = async ()=>{
+    //         const snap = await getDocs(collection(teamdata,"team"));
+    //         await setTeam(snap.docs);
+    //         console.log(snap.docs[0].data());
             
-        }
-        getTeams();
+    //     }
+    //     getTeams();
 
-    },[])
+    // },[])
   return (
     <section className='md:p-10'>
     <div className='m-6 lg:mx-20'>
@@ -25,7 +25,7 @@ export default function Team() {
     <h6 className='text-gray-700'>দিনহাটা এবং আশেপাশের এলাকার মানুষের জীবনযাত্রার মান উন্নয়নে আমরা, দায়বদ্ধ ওয়েলফেয়ার সোসাইটি, একদল স্বপ্নবান এবং উদ্যমী মানুষ একত্রিত হয়েছি। আমাদের টিমের প্রত্যেক সদস্য তাঁদের নিজ নিজ কর্মক্ষেত্রে দক্ষ এবং সমাজের উন্নয়নে একনিষ্ঠভাবে কাজ করে চলেছেন। আমরা শিক্ষার প্রসার, সচেতনতা বৃদ্ধি, এবং অসহায় মানুষদের সহায়তা প্রদানে প্রতিশ্রুতিবদ্ধ। প্রত্যেক সদস্যের নেতৃত্ব, নিষ্ঠা এবং সেবার মানসিকতা আমাদের সংগঠনকে প্রতিদিন আরও শক্তিশালী করে তুলছে।</h6>
     </div>
     <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-x-6 gap-y-16 text-center mt-24 px-20'>
-        {
+        {/* {
             teamMembers.map((members,index)=>(
                 <div key={index} className="  relative rounded hover:scale-105 hover:shadow-xl shadow-inner transition-all duration-300 border ">
                
@@ -64,7 +64,7 @@ export default function Team() {
                 </div>
 
             ))
-        }
+        } */}
         </div>
     </section>
   )
